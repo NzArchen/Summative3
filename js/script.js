debe6d5beea958a59e49055ab7d88b957bb07615
Chenqi Yue <845863436@cit3-501m-06.auckland.natcoll.ac.nz>
2017-09-27 15:52:07 +1300

    crate a gallery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,58 @@
 $(function() {
 
   // jquery coding inside this function
-  
+  let key = 'VMNZH093QVRWvJhDd5cMBVFv30qgV2Bi'; //form my behance account api
+
+	let urlProjects = 'https://api.behance.net/v2/users/Carlaveggio/projects?client_id='+key;
+	console.log(urlProjects);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Masonry grid setup
+  $(".grid").masonry({
+    itemSelector: ".grid__item",
+    columnWidth: ".grid__sizer",
+    // gutter: 10,
+    gutter: 23,
+    percentPosition: true,
+    fitWidth: true,
+  });
+  // Image replacement handler
+  $(document).on("click", ".js-button", function() {
+    var imageSrc = $(this).parents(".grid__item").find("img").attr("src");
+    $(".js-download").attr("href", imageSrc);
+    $(".js-modal-image").attr("src", imageSrc);
+    $(document).on("click", ".js-heart", function() {
+      $(this).toggleClass("active");
+    });
+  });
 
 });
+
